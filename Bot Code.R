f02063230ed6587624061d03ddaf2bdfa5e9f539
timweichong <timlim@wharton.upenn.edu>
2017-06-12 17:53:48 +0200

    Fixed some bugs.

--- Bot Code.R
+++ Bot Code.R
@@ -10,7 +10,7 @@ access_token = "389902757-l6jrNY4hhZRsBRYFjYMPbGAHnHrd4RSy58tdxzwN"
 access_secret = "foo4jcyrLrwYfEocHdMjYJlVsrP9RgxSueprZSreP9pUB"
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
-tweets = searchTwitter("trump", n = 1000, lang = "en", since = today())
+tweets = searchTwitter("trump", n = 1000, lang = "en")
 twitter.df = do.call(rbind, lapply(tweets, function(x) x$toDataFrame()))
 twitter.text = sapply(tweets, function(x) x$getText())
 twitter.text= gsub("[^[:print:]]", "", twitter.text)
@@ -20,11 +20,11 @@ twitter.df$text = twitter.text
 twitter.df = select(twitter.df, text) %>% mutate(tweet = 1:nrow(.))
 twitter.tiny = twitter.df %>% unnest_tokens(word,text) %>% anti_join(stop_words) %>% 
   inner_join(get_sentiments("afinn")) %>% group_by(tweet) %>% mutate(sentsum = sum(score)) %>% 
-  select(tweet, sentsum) %>% mutate(sentiment = if_else(sentsum>0, 'positive', 'negative', missing = 'neutral'))
+  select(tweet, sentsum) %>% mutate(sentiment = if_else(sentsum>0, 'positive', 'negative'))
 
-npositive = nrow(twitter.tiny[twitter.tiny$sentiment =='positive',])
-nnegative = nrow(twitter.tiny[twitter.tiny$sentiment =='negative',])
+npositive = nrow(unique(twitter.tiny[twitter.tiny$sentiment =='positive',]))
+nnegative = nrow(unique(twitter.tiny[twitter.tiny$sentiment =='negative',]))
 nneutral = 1000 - npositive - nnegative
 
 
-cat(sprintf("Of latest 1000 tweets about Trump today: %d are positive, %d are negative, and %d are neutral.", npositive, nnegative, nneutral))
+tweet(sprintf("1000 tweets about Trump today: %d positive, %d negative, & %d neutral.", npositive, nnegative, nneutral))
